f704bc468111b28bbf30f32c2781d554735eb45e
amarok36 <bobrowww@mail.ru>
2025-05-30 18:43:40 +0500

    Changing the printLine method

--- src/Main.java
+++ src/Main.java
@@ -86,6 +86,9 @@ public class Main {
     }
 
     static void printLine() {
-        System.out.println("* * * * * * * * * * * * * * * * * * * *");
+        for (int i = 0; i < 20; i++) {
+            System.out.print("* ");
+        }
+        System.out.println();
     }
 }
